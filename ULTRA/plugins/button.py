f0e3d541633910fc531218fc73755d6af549aebb
Imteyaz_king <79402181+Imteyazking@users.noreply.github.com>
2021-06-29 16:54:12 +0530

    Update button.py

--- ULTRA/plugins/button.py
+++ ULTRA/plugins/button.py
@@ -1,6 +1,6 @@
 # jnl
-# COPYRIGHT (C) 2021-2022 BY LEGENDX22
-# MADE BY LEGENDX22
+# COPYRIGHT (C) 2021-2022 BY IMTEYAZ
+# MADE BY IMTEYAZ
 from telethon import Button, events
 from ..utils import admin_cmd
 from ..utils import edit_or_reply, eor
@@ -14,10 +14,10 @@ async def Buttons(event):
     ULTRAX = Var.TG_BOT_USER_NAME_BF_HER
     pro = event.text[7:]
     pro, boy = pro.split("|")
-    f = open("Button.txt", "w") # by LEGENDX22, PROBOYX
+    f = open("Button.txt", "w") # by Imteyaz_king, PROBOYX
     f.write(f'{pro}\n{boy}')
     f.close()
-    LEGENDX = await bot.inline_query(ULTRAX, "BUTTON")
+    KINGFIGHTER = await bot.inline_query(KINGFIGHTER, "BUTTON")
     await LEGENDX[0].click(event.chat_id)
     await event.delete()
 
@@ -29,14 +29,14 @@ async def file(event):
   PROBOYX = open("Button.txt")
   bc = PROBOYX.readlines()[1]
   PROBOYX.close()
-  LEGENDX = event.builder
+  KINGFIGHTER = event.builder
   LEGENDX22 = [[Button.url(f'{ok}', f'{bc}')]]
-  PROBOYXOP = LEGENDX.article(title='Button by ULTRA X', text=f'{ok}', buttons=LEGENDX22)
+  PROBOYXOP = LEGENDX.article(title='Button by KING FIGHTER', text=f'{ok}', buttons=Imteyaz_king)
   await event.answer([PROBOYXOP])
 
 CMD_HELP.update
 (
   {
-    "button": ".button <button name>|<link>\n`.button UltraX|https://t.me/ULTRAXOT`\nmake sure your name and link no have Useless space"
+    "button": ".button <button name>|<link>\n`.button KINGFIGHTER|https://t.me/King_fighter_Bot_support`\nmake sure your name and link no have Useless space"
   }
 )
